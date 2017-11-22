7e9d34ff26e4aff19ad482ad379ca066f2fe7f3b
Gavin Parker <gp14958@my.bristol.ac.uk>
2017-11-22 16:55:14 +0000

    Use scheduler instead of loop

--- choptop-backend/choptop.py
+++ choptop-backend/choptop.py
@@ -1,12 +1,12 @@
 from sensor import Sensor
 import RPi.GPIO as GPIO
-import time
+import time, sched
 import sys
 
 class ChopTop:
 	def __init__(self):
 		self.finger_position = (0,0)
-	def main(self):
+	def start(self):
 		sensor_1 = Sensor(20, 21)
 		sensor_1.start()
 		time.sleep(0.5)
@@ -16,25 +16,22 @@ class ChopTop:
 		timestr = timestr + '.log'
 		log_file = open(timestr,"w")
 		sys.stdout = log_file
-		try:
-			while True:
-				frame_start = int(round(time.time() * 1000))
-				sensor_1.update()
-				#get weights for each sensor
-				if sensor_1.buffer.count > 0:
-					weight = sensor_1.buffer.pop()
-				print weight + ", " + int(round(time.time() * 1000))
-				self.finger_position = calculatePosition([weight])
+        self.scheduler = sched.scheduler(time.time, time.sleep)
+        self.scheduler.enter(10, 1, update, (self, sc))
+        self.scheduler.run()
 
-				frame_time = int(round(time.time() * 1000)) - frame_start
-				if frame_time_millis > frame_time:
-					time.sleep(frame_time_millis - frame_time)
-				#print "something"
-			sys.stdout = old_stdout
-			log_file.close()
-
-		except:
-			GPIO.cleanup() 
+def update(self, sc):
+    try:
+        sensor_1.update()
+        #get weights for each sensor
+        if sensor_1.buffer.count > 0:
+            weight = sensor_1.buffer.pop()
+        print weight + ", " + int(round(time.time() * 1000))
+        self.finger_position = calculatePosition([weight])
+        self.scheduler.enter(10, 1, update, (self, sc))
+        self.scheduler.run()
+    except:
+        GPIO.cleanup()
 
 def calculatePosition(weights):
 	return (0,0)
@@ -42,7 +39,7 @@ def calculatePosition(weights):
 def main():
 	# my code here
 	choptop = ChopTop()
-	choptop.main()
+	choptop.start()
 
 if __name__ == "__main__":
 		main()
