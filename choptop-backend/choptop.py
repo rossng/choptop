f51f4c65a0300a765125473d9f3626b58f376504
Gavin Parker <gp14958@my.bristol.ac.uk>
2017-11-22 16:05:59 +0000

    Consistent 80Hz read rate

--- choptop-backend/choptop.py
+++ choptop-backend/choptop.py
@@ -9,14 +9,20 @@ class ChopTop:
         sensor_1 = Sensor(20, 21)
         sensor_1.start()
         time.sleep(0.5)
+        frame_time_millis = (1/80)*1000
         try:
             while True:
+                frame_start = int(round(time.time() * 1000))
                 sensor_1.update()
                 #get weights for each sensor
                 if sensor_1.buffer.count > 0:
                     weight = sensor_1.buffer.pop()
                 print weight
                 self.finger_position = calculatePosition([weight])
+
+                frame_time = int(round(time.time() * 1000)) - frame_start
+                print 1/frame_time
+                time.sleep(frame_time_millis - frame_time)
         except:
             GPIO.cleanup() 
 
