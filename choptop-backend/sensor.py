90286612f18b4545300d24b7c682ced7644914d2
Gavin Parker <gp14958@my.bristol.ac.uk>
2017-11-22 14:44:44 +0000

    use threaded queue for sensor data

--- choptop-backend/sensor.py
+++ choptop-backend/sensor.py
@@ -1,13 +1,25 @@
+import threading
+import Queue
 class Sensor:
     def __init__(self, dout, pd_sck):
-    load_sensor = HX711(dout, pd_sck)
-    load_sensor.set_reading_format("LSB", "MSB")
-    load_sensor.set_reference_unit(92)
-    load_sensor.reset()
-    load_sensor.tare()
-    load_sensor.power_up()
-    self.load_sensor = load_sensor
+        load_sensor = HX711(dout, pd_sck)
+        load_sensor.set_reading_format("LSB", "MSB")
+        load_sensor.set_reference_unit(92)
+        load_sensor.reset()
+        load_sensor.tare()
+        load_sensor.power_up()
+        self.load_sensor = load_sensor
 
-    def get_weight(self):
-        return self.get_weight(1)
+    def start(self):
+        self.queue = Queue()
+        self.thread = threading.Thread(target = self.collectStream)
+        self.thread.start()
+
+    def getWeight(self):
+        return self.load_sensor.get_weight(1)
+    
+    def collectStream(self):
+        while True:
+            weight = self.load_sensor.get_weight(1)
+            self.queue.put(weight)
 
