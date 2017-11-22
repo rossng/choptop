f29c85cc24db26d417acd4e6df1f784779e7e278
Tuana Celik <tc13007@my.bristol.ac.uk>
2017-11-22 16:35:36 +0000

    no message

--- choptop-backend/choptop.py
+++ choptop-backend/choptop.py
@@ -11,12 +11,12 @@ class ChopTop:
         sensor_1.start()
         time.sleep(0.5)
         frame_time_millis = int((1/80)*1000)
+		old_stdout = sys.stdout
+		timestr = time.strftime("%Y%m%d-%H%M%S")
+		timestr = timestr + '.log'
+		log_file = open(timestr,"w")
+		sys.stdout = log_file
         try:
-			old_stdout = sys.stdout
-			timestr = time.strftime("%Y%m%d-%H%M%S")
-			timestr = timestr + '.log'
-			log_file = open(timestr,"w")
-			sys.stdout = log_file
             while True:
                 frame_start = int(round(time.time() * 1000))
                 sensor_1.update()
