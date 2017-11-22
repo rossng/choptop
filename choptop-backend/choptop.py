73e7c2b82faf18ccfbd2555a6d8d35c271bb415b
Tuana Celik <tc13007@my.bristol.ac.uk>
2017-11-22 16:53:41 +0000

    no message

--- choptop-backend/choptop.py
+++ choptop-backend/choptop.py
@@ -23,8 +23,8 @@ class ChopTop:
 				#get weights for each sensor
 				if sensor_1.buffer.count > 0:
 					weight = sensor_1.buffer.pop()
-				print (weight, end='')
-				print (", ", end='')
+				print weight,
+				print ", ",
 				print int(round(time.time() * 1000)))
 				self.finger_position = calculatePosition([weight])
 
