1eccc496f71056e96730fbf6ade4e4859acbe982
Ross Gardiner <ross-public@gmx.com>
2017-11-29 21:57:10 +0000

    Reformat, comment out signal handler

--- choptop-backend-2/src/hx711.cpp
+++ choptop-backend-2/src/hx711.cpp
@@ -7,163 +7,163 @@
 #include <signal.h>
 #include <vector>
 #include "hx711.h"
+
 using std::thread;
 float sensor_inputs[] = {0.0f, 0.0f, 0.0f, 0.0f};
 thread threads[4];
 
 HX711::HX711(uint8_t clockPin, uint8_t dataPin, uint8_t skipSetup) :
-	mGainBits(1),
-	mScale(1.0f),
-	mOffset(0),
-	mClockPin(clockPin),
-	mDataPin(dataPin)
-{
-	this->initialize(skipSetup);
+        mGainBits(1),
+        mScale(1.0f),
+        mOffset(0),
+        mClockPin(clockPin),
+        mDataPin(dataPin) {
+    this->initialize(skipSetup);
 }
 
-void HX711::initialize(uint8_t skipSetup){
-	if((!skipSetup) && wiringPiSetupGpio() == -1){
-		printf("initialization failed");
-	}
-	pinMode(mClockPin, OUTPUT);
-	pinMode(mDataPin, INPUT);
+void HX711::initialize(uint8_t skipSetup) {
+    if ((!skipSetup) && wiringPiSetupGpio() == -1) {
+        printf("initialization failed");
+    }
+    pinMode(mClockPin, OUTPUT);
+    pinMode(mDataPin, INPUT);
 }
 
-bool HX711::isReady(){
-	return digitalRead(mDataPin) == LOW;
+bool HX711::isReady() {
+    return digitalRead(mDataPin) == LOW;
 }
 
-void HX711::setGain(uint8_t gain){
-	switch(gain){
-		case GAIN_128:
-			this->mGainBits = 1;
-		break;
-		case GAIN_64:
-			this->mGainBits = 3;
-		break;
-		case GAIN_32:
-			this->mGainBits = 2;
-		break;
-		default:
-			//invalid gain, ignore
-		break;
-	}
+void HX711::setGain(uint8_t gain) {
+    switch (gain) {
+        case GAIN_128:
+            this->mGainBits = 1;
+            break;
+        case GAIN_64:
+            this->mGainBits = 3;
+            break;
+        case GAIN_32:
+            this->mGainBits = 2;
+            break;
+        default:
+            //invalid gain, ignore
+            break;
+    }
 
-	digitalWrite(mClockPin, LOW);
-	read();
+    digitalWrite(mClockPin, LOW);
+    read();
 }
 
 int32_t HX711::read() {
-	// wait for the chip to become ready
-	while (!this->isReady());
+    // wait for the chip to become ready
+    while (!this->isReady());
 
-	int32_t data = 0;
-	// pulse the clock pin 24 times to read the data
-	for(uint8_t i = 24; i--;){
-		digitalWrite(mClockPin, HIGH);
+    int32_t data = 0;
+    // pulse the clock pin 24 times to read the data
+    for (uint8_t i = 24; i--;) {
+        digitalWrite(mClockPin, HIGH);
 
-		digitalRead(mDataPin);
-		data |= (digitalRead(mDataPin) << i);
+        digitalRead(mDataPin);
+        data |= (digitalRead(mDataPin) << i);
 
-		digitalWrite(mClockPin, LOW);
-	}
+        digitalWrite(mClockPin, LOW);
+    }
 
-	// set the channel and the gain factor for the next reading using the clock pin
-	for (int i = 0; i < mGainBits; i++) {
-		digitalWrite(mClockPin, HIGH);
-		digitalWrite(mClockPin, LOW);
-	}
+    // set the channel and the gain factor for the next reading using the clock pin
+    for (int i = 0; i < mGainBits; i++) {
+        digitalWrite(mClockPin, HIGH);
+        digitalWrite(mClockPin, LOW);
+    }
 
-	if(data & 0x800000){
-		data |= (long) ~0xffffff;
-	}
+    if (data & 0x800000) {
+        data |= (long) ~0xffffff;
+    }
 
-	return data;
+    return data;
 }
 
 int32_t HX711::readAverage(uint8_t times) {
-	int64_t sum = 0;
-	for (uint8_t i = 0; i < times; i++) {
-		sum += read();
-	}
-	return sum / times;
+    int64_t sum = 0;
+    for (uint8_t i = 0; i < times; i++) {
+        sum += read();
+    }
+    return sum / times;
 }
 
 int32_t HX711::getRawValue(uint8_t times) {
-	return readAverage(times) - mOffset;
+    return readAverage(times) - mOffset;
 }
 
 float HX711::getUnits(uint8_t times) {
-	return getRawValue(times) / mScale;
+    return getRawValue(times) / mScale;
 }
 
 void HX711::tare(uint8_t times) {
-	uint64_t sum = readAverage(times);
-	setOffset(sum);
+    uint64_t sum = readAverage(times);
+    setOffset(sum);
 }
 
 void HX711::setScale(float scale) {
-	mScale = scale;
+    mScale = scale;
 }
 
 void HX711::setOffset(int32_t offset) {
-	mOffset = offset;
+    mOffset = offset;
 }
 
 void HX711::powerDown() {
-	digitalWrite(mClockPin, LOW);
-	digitalWrite(mClockPin, HIGH);
+    digitalWrite(mClockPin, LOW);
+    digitalWrite(mClockPin, HIGH);
 }
 
 void HX711::powerUp() {
-	digitalWrite(mClockPin, LOW);
+    digitalWrite(mClockPin, LOW);
 }
 
-int32_t HX711::getOffset(){
-	return this->mOffset;
+int32_t HX711::getOffset() {
+    return this->mOffset;
 }
 
-float HX711::getScale(){
-	return this->mScale;
+float HX711::getScale() {
+    return this->mScale;
 }
 
-void graceful_shutdown(sig_t s){
-    for(int i=0; i < 4; i++){
+void graceful_shutdown(sig_t s) {
+    for (int i = 0; i < 4; i++) {
         threads[i].join();
     }
     exit(1);
 }
 
-void getReadings(int clk, int data, int index){
+void getReadings(int clk, int data, int index) {
     HX711 sensor(clk, data, 0);
     sensor.tare();
     sensor.setScale(16000);
-    while(true){
+    while (true) {
         sensor_inputs[index] = sensor.getUnits();
     }
 }
 
-float clamp(float n, float hi, float lo){
-	return std::min(std::max(n, lo), hi);
+float clamp(float n, float hi, float lo) {
+    return std::min(std::max(n, lo), hi);
 }
 
-int main(){
-    threads[0] = thread(getReadings, 6, 5, 0);     
+int main() {
+    threads[0] = thread(getReadings, 6, 5, 0);
     threads[1] = thread(getReadings, 8, 7, 1);
-	threads[2] = thread(getReadings, 10, 9, 2);
-	threads[3] = thread(getReadings, 21, 20, 3);
-	
-    signal(SIGINT, graceful_shutdown);
+    threads[2] = thread(getReadings, 10, 9, 2);
+    threads[3] = thread(getReadings, 21, 20, 3);
+
+    //signal(SIGINT, graceful_shutdown);
     float weights[] = {0.0f, 0.0f, 0.0f, 0.0f};
-	while(true){
+    while (true) {
         float total = 0;
-        for(int i=0; i<4; i++){
+        for (int i = 0; i < 4; i++) {
             weights[i] = sensor_inputs[i];
             total += weights[i];
         }
         float x = clamp((weights[0] + weights[1]) / total, 0.0, 1.0f);
         float y = clamp((weights[1] + weights[2]) / total, 0.0, 1.0f);
         std::cout << x << "," << y << std::endl;
-	}
+    }
 }
 
