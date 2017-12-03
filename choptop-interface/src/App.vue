9d926da2166e602c4f075e95240c40df44765cb4
Ross Gardiner <ross-public@gmx.com>
2017-12-03 22:01:44 +0000

    Remove hello world, reformat a bit

--- choptop-interface/src/App.vue
+++ choptop-interface/src/App.vue
@@ -4,7 +4,7 @@
     <NavButton direction="left" :pressed="latestUpdate.leftPressed"/>
     <div id="main">
       <WelcomeToChopTop v-if="shouldShowWelcome()"/>
-      <RecipeListing v-if="shouldShowRecipeListing()" :recipes="recipe" />
+      <RecipeListing v-if="shouldShowRecipeListing()" :recipes="recipe"/>
     </div>
     <NavButton direction="right" :pressed="latestUpdate.rightPressed"/>
     <NavButton direction="down" :pressed="latestUpdate.downPressed"/>
@@ -14,79 +14,76 @@
 </template>
 
 <script>
-import WelcomeToChopTop from './components/WelcomeToChopTop'
-import NavButton from './components/NavButton'
-import RecipeListing from './components/RecipeListing'
+  import WelcomeToChopTop from './components/WelcomeToChopTop'
+  import NavButton from './components/NavButton'
+  import RecipeListing from './components/RecipeListing'
 
-import VueWebsocket from 'vue-native-websocket'
+  import VueWebsocket from 'vue-native-websocket'
 
-import Vue from 'vue'
+  import Vue from 'vue'
 
-Vue.use(VueWebsocket, "ws://localhost:8765");
-
-
-
-// import recipes json file
-import Recipes from './recipes.json'
-
-export default {
-  name: 'app',
-  components: {
-    WelcomeToChopTop,
-    NavButton,
-    RecipeListing
-  },
-
-  data (){
-    return {
-      currentScreen:"recipeListing",
-      recipe: Recipes,
+  Vue.use(VueWebsocket, "ws://localhost:8765");
 
 
+  // import recipes json file
+  import Recipes from './recipes.json'
 
-      //data fetching from server
-      error: null,
-      latestUpdate: {}
-
-
-
-    }
-  },
-
-  created () {
-    // fetch the data when the view is created and the data is
-    // already being observed
-    this.fetchData()
-  },
-
-
-  methods: {
-    fetchData () {
-      this.error = this.update = null
-      this.loading = true
-      this.$options.sockets.onmessage = (data) => console.log(data)
-      // replace `getupdate` with your data fetching util / API wrapper
-      // $.get("http://localhost:8432", (update, err) => {
-      //   this.loading = false
-      //   if (err.toString() != "success") {
-      //     this.error = err.toString()
-      //   } else {
-      //     this.latestUpdate = update
-      //   }
-      // })
+  export default {
+    name: 'app',
+    components: {
+      WelcomeToChopTop,
+      NavButton,
+      RecipeListing
     },
-    shouldShowWelcome: function() {
-      return this.currentScreen === "welcome"
 
+    data() {
+      return {
+        currentScreen: "recipeListing",
+        recipe: Recipes,
+
+
+        //data fetching from server
+        error: null,
+        latestUpdate: {}
+
+
+      }
     },
-    shouldShowRecipeListing: function(){
-      return this.currentScreen === "recipeListing"
-    }
-  },
-  socket: {
+
+    created() {
+      // fetch the data when the view is created and the data is
+      // already being observed
+      this.fetchData()
+    },
+
+
+    methods: {
+      fetchData() {
+        this.error = this.update = null
+        this.loading = true
+        this.$options.sockets.onmessage = (data) => console.log(data)
+        // replace `getupdate` with your data fetching util / API wrapper
+        // $.get("http://localhost:8432", (update, err) => {
+        //   this.loading = false
+        //   if (err.toString() != "success") {
+        //     this.error = err.toString()
+        //   } else {
+        //     this.latestUpdate = update
+        //   }
+        // })
+      },
+      shouldShowWelcome: function () {
+        return this.currentScreen === "welcome"
+
+      },
+      shouldShowRecipeListing: function () {
+        return this.currentScreen === "recipeListing"
+      }
+    },
+    socket: {
       // Prefix for event names
       // prefix: "/counter/",
-      
+
       // If you set `namespace`, it will create a new socket connection to the namespace instead of `/`
       // namespace: "/counter",
 
@@ -98,8 +95,8 @@ export default {
         changed(msg) {
           console.log("Something changed: " + msg);
         },
-        
-         // common socket.io events
+
+        // common socket.io events
         connect() {
           console.log("Websocket connected to " + this.$socket.nsp);
         },
@@ -111,36 +108,31 @@ export default {
         error(err) {
           console.error("Websocket error!", err);
         }
-        
+
 
       }
     }
 
 
-
-
-}
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  height:100%;
-  width:100%;
-  /*overflow:hidden;*/
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    height: 100%;
+    width: 100%;
+    /*overflow:hidden;*/
+  }
 
-
-
-
-#main{
-  width:760px;
-  height:460px;
-  float:left;
-  display: inline-block;
-}
+  #main {
+    width: 760px;
+    height: 460px;
+    float: left;
+    display: inline-block;
+  }
 </style>
--- choptop-interface/src/main.js
+++ choptop-interface/src/main.js
@@ -3,13 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-require('../static/globalStyles.css')
+require('../static/globalStyles.css');
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
   components: { App }
-})
+});
