8ea6ca3a1c8d3715076c65f23bc446c3f9a8bdb4
simonjpartridge <me@simonjpartridge.co.uk>
2017-12-09 20:31:38 +0000

    ooooh v fancy timers

--- choptop-interface/src/components/Timer.vue
+++ choptop-interface/src/components/Timer.vue
@@ -1,13 +1,34 @@
 <template>
 	<div class="timer">
-		{{name}} {{timeString(currentTime)}} of {{timeString(startTime)}}
+		<vue-circle
+		ref="circ"
+        :progress="100"
+        :size="100"
+        :reverse="false"
+        line-cap="round"
+        inner-text="getTimeString()"
+        show-percent="false"
+        :animation-start-value="0"
+        :start-angle="(3/2)*Math.PI"
+        insert-mode="append"
+        :thickness="10"
+        :show-percent="false">
+          <span class="timerValue">{{getTimeString()}}</span>
+          <span class="timerName">{{name}}</span>
+          <span class="totalTime">{{timeString(startTime)}}</span>
+      </vue-circle>
 	</div>
 </template>
 
 <script>
+	import VueCircle from 'vue2-circle-progress';
+
 	export default {
 	  name: 'timer',
 	  props:['eventBus', 'running', 'startTime', 'name'],
+	  components:{
+		VueCircle
+	  },
 	  data () {
 	    return {
 	      currentTime:0,
@@ -22,6 +43,11 @@
 	  		if(!this.running || this.currentTime <=0) return;
 
 	  		this.currentTime -= 1/60;
+	  		if(this.currentTime <=0){
+	  			this.$refs.circ.updateProgress(0.01)	
+	  		}else{
+	  			this.$refs.circ.updateProgress((this.currentTime/this.startTime)*100)	
+	  		}
 	  	},
 	  	timeString(minutes){
 	  		if(minutes < 0) minutes = 0;
@@ -34,6 +60,9 @@
 
 	  		return mins + ":" + secsString
 
+	  	},
+	  	getTimeString(){
+	  		return this.timeString(this.currentTime);
 	  	}
 	  }
 	}
@@ -41,13 +70,30 @@
 
 
 
-<style scoped>
+<style>
 	.timer{
-		position: fixed;
 		bottom: 45px;
 		text-align: center;
 		width: 800px;
 		left: 200px;
+	}
+
+	.inner-text{
+		color:red;
+	}
+
+	.timerValue{
+		color:red;
+		margin-top:-12px;
+	}
+	.timerName{
+
+		color:blue;
+	}
+
+	.totalTime{
+		font-size:70%;
+		margin-top:10px;
 	}
 	
 </style>
