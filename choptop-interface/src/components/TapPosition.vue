fc87288a9e796e8979d221ae205becb90d2c404e
Orsi Lukacs <ol14474@my.bristol.ac.uk>
2017-12-11 22:02:54 +0000

    X-Y fixed

--- choptop-interface/src/components/TapPosition.vue
+++ choptop-interface/src/components/TapPosition.vue
@@ -27,7 +27,7 @@
 	  		if (this.latestPos ==null){
 	  			return ''; 
 	  		}
-	  		text += "transform: translate("+this.latestPos[0]*800+"px,"+ this.latestPos[1]*480+"px) scale("+this.latestScale+")";
+	  		text += "transform: translate("+this.latestPos[0]*800+"px,"+ (1.0-this.latestPos[1])*480+"px) scale("+this.latestScale+")";
 	  		return text;
 	  	}
 	  },
