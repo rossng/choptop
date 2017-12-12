608d9789f7baca70bee2ba9b4f96c71705ecf3e6
Ross Gardiner <ross-public@gmx.com>
2017-12-12 14:24:50 +0000

    Delete hasWeight text from StepDisplay.vue

--- choptop-interface/src/components/StepDisplay.vue
+++ choptop-interface/src/components/StepDisplay.vue
@@ -7,9 +7,6 @@
       <div class="step">
         <p class="stepCount">Step {{stepIdx+1}} of {{steps.length}}</p>
         {{steps[stepIdx].text}}
-        <div v-if="hasWeight()" class="weigher">
-          hasWeight
-        </div>
       </div>
       <div :class="{rightArr:true, arr:true, noshow: stepIdx === steps.length - 1}">
         <icon name="angle-right" scale="4"></icon>
